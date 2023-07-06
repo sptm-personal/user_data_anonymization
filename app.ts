cda7ba897ec96aa4c6d8af2ba9da3ec6308742ab
sptm <me@sptm.dev>
2023-07-06 21:32:45 +0400

    update app.ts

--- app.ts
+++ app.ts
@@ -8,33 +8,35 @@ import { CustomerModel } from './models/CustomerModel';
 
 const INTERVAL_TIME_IN_MS = 200;
 
-const generateCustomer = (): Customer => ({
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
-  email: faker.internet.email(),
-  address: {
-    line1: faker.location.streetAddress(true),
-    line2: faker.location.secondaryAddress(),
-    postcode: faker.location.zipCode(),
-    city: faker.location.city(),
-    state: faker.location.state({ abbreviated: true }),
-    country: faker.location.countryCode()
-  },
-  createdAt: faker.date.past().toISOString()
-});
+class App {
+  static generateCustomer = (): Customer => ({
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    address: {
+      line1: faker.location.streetAddress(true),
+      line2: faker.location.secondaryAddress(),
+      postcode: faker.location.zipCode(),
+      city: faker.location.city(),
+      state: faker.location.state({ abbreviated: true }),
+      country: faker.location.countryCode()
+    },
+    createdAt: faker.date.past().toISOString()
+  });
 
-const generateCustomers = (): Customer[] => {
-  const quantity = Math.floor(Math.random() * 10) + 1;
-  return Array.apply(null, Array(quantity)).map(() => generateCustomer());
-};
+  static generateCustomers = (): Customer[] => {
+    const quantity = Math.floor(Math.random() * 10) + 1;
+    return Array.apply(null, Array(quantity)).map(() => this.generateCustomer());
+  };
 
-const pushCustomersToDb = async (): Promise<void> => {
-  logger.info('Pushing customers to DB');
-  const customers = generateCustomers();
-  await CustomerModel.insertMany(customers);
-};
+  static pushCustomersToDb = async (): Promise<void> => {
+    logger.info('Pushing customers to DB');
+    const customers = this.generateCustomers();
+    await CustomerModel.insertMany(customers);
+  };
+}
 
 (async () => {
   await connect(process.env.DB_URI as string);
-  setInterval(pushCustomersToDb, INTERVAL_TIME_IN_MS);
+  setInterval(App.pushCustomersToDb, INTERVAL_TIME_IN_MS);
 })();
